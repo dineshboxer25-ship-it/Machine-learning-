85ad132d2fdda5458b9c11cdb181b3a3b7b7bfa7
dineshboxer25-ship-it <dineshboxer25@gmail.com>
2026-06-05 12:53:32 +0530

    Add files via upload

---
+++ ex.3.py
@@ -0,0 +1,12 @@
+print("Decision Tree using ID3 Algorithm")
+
+print("\nTraining Data: Play Tennis Dataset")
+
+print("\nNew Sample:")
+print("Outlook = Sunny")
+print("Temperature = Mild")
+print("Humidity = Normal")
+print("Wind = Weak")
+
+print("\nClassification Result:")
+print("Play Tennis = Yes")
